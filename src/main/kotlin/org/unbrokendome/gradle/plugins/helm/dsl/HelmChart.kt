908d67747f57aac7f8300bc2c66768e658ce636a
Till Krullmann <till.krullmann@external.takeaway.com>
2018-08-25 22:51:09 +0200

    Make HelmChart implement Buildable

--- src/main/kotlin/org/unbrokendome/gradle/plugins/helm/dsl/HelmChart.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/helm/dsl/HelmChart.kt
@@ -1,5 +1,6 @@
 package org.unbrokendome.gradle.plugins.helm.dsl
 
+import org.gradle.api.Buildable
 import org.gradle.api.Named
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
@@ -7,6 +8,8 @@ import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.ProjectLayout
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.TaskDependency
+import org.unbrokendome.gradle.plugins.helm.rules.packageTaskName
 import org.unbrokendome.gradle.plugins.helm.util.property
 import javax.inject.Inject
 
@@ -14,7 +17,7 @@ import javax.inject.Inject
 /**
  * Represents a Helm chart built by this project.
  */
-interface HelmChart : Named {
+interface HelmChart : Named, Buildable {
 
     /**
      * The chart name.
@@ -56,6 +59,17 @@ private open class DefaultHelmChart
 
     override val sourceDir: DirectoryProperty =
             projectLayout.directoryProperty()
+
+
+    override fun getBuildDependencies(): TaskDependency =
+            TaskDependency { task ->
+                if (task != null) {
+                    setOf(
+                            task.project.tasks.getByName(packageTaskName))
+                } else {
+                    emptySet()
+                }
+            }
 }
 
 
