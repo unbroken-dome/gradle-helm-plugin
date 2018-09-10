1af15993671c7461a60ba52339859cd9d5056507
Till Krullmann <till.krullmann@gmx.net>
2018-09-10 16:19:12 +0200

    Change the type of the "chart" property on HelmInstall to `String` (`Any` is problematic because of overload resolution ambiguity in Kotlin)
    Also, add "repository" property corresponding to the `--repo` CLI parameter

--- src/main/kotlin/org/unbrokendome/gradle/plugins/helm/command/tasks/HelmInstall.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/helm/command/tasks/HelmInstall.kt
@@ -4,6 +4,7 @@ import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.Directory
 import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
@@ -25,14 +26,48 @@ open class HelmInstall : AbstractHelmServerCommandTask() {
     /**
      * The chart to be installed. This can be any of the forms accepted by the Helm CLI.
      *
-     * - chart reference (`String`): e.g. `stable/mariadb`
-     * - path to a packaged chart (`String`, [File], [RegularFile])
-     * - path to an unpacked chart directory (`String`, [File], [Directory])
-     * - absolute URL (`String`, [URI]): e.g. `https://example.com/charts/nginx-1.2.3.tgz`
-     * - chart reference and repository URL
+     * - chart reference: e.g. `stable/mariadb`
+     * - path to a packaged chart
+     * - path to an unpacked chart directory
+     * - absolute URL: e.g. `https://example.com/charts/nginx-1.2.3.tgz`
+     * - simple chart reference, e.g. `mariadb` (you must also set the [repository] property in this case)
      */
     @get:Input
-    val chart: Property<Any> =
+    val chart: Property<String> =
+            project.objects.property()
+
+
+    /**
+     * Sets the chart to be installed. The value can be any of the forms accepted by the Helm CLI.
+     *
+     * This is a convenience method that can be used instead of setting the [chart] property directly.
+     *
+     * The following argument types are accepted:
+     *
+     * - A chart reference (`String`): e.g. `stable/mariadb`.
+     * - A path to a packaged chart (`String`, [File], [RegularFile])
+     * - A path to an unpacked chart directory (`String`, [File], [Directory])
+     * - An absolute URL (`String`, [URI]): e.g. `https://example.com/charts/nginx-1.2.3.tgz`
+     * - A simple chart reference (`String`), e.g. `mariadb`.
+     *   Note that you must also set the [repository] property in this case.
+     * - a [Provider] of any of the above.
+     *
+     */
+    fun from(chart: Any) {
+        if (chart is Provider<*>) {
+            this.chart.set(chart.map { it.toString() })
+        } else {
+            this.chart.set(chart.toString())
+        }
+    }
+
+
+    /**
+     * Chart repository URL where to locate the requested chart. Corresponds to the `--repo` Helm CLI parameter.
+     *
+     * Use this when the [chart] property contains only a simple chart reference, without a symbolic repository name.
+     */
+    val repository: Property<URI> =
             project.objects.property()
 
 
