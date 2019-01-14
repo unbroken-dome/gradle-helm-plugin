9fa5743a0e3d098acad9dae249b336d82e25b76b
Till Krullmann <till.krullmann@gmx.net>
2019-01-14 12:46:18 +0100

    add Input Optional annotations to "repository" property

--- src/main/kotlin/org/unbrokendome/gradle/plugins/helm/command/tasks/HelmInstall.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/helm/command/tasks/HelmInstall.kt
@@ -6,11 +6,7 @@ import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.Optional
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.*
 import org.unbrokendome.gradle.plugins.helm.command.valuesOptions
 import org.unbrokendome.gradle.plugins.helm.util.mapProperty
 import org.unbrokendome.gradle.plugins.helm.util.property
