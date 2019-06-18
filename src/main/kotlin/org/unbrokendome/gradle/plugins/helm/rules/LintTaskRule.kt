5e6f4b16ca799e0d9f94d4354f7a1e0ad14aca73
Till Krullmann <till.krullmann@gmx.net>
2019-06-18 17:05:28 +0200

    Fix: lint task should depend on helmInitClient
    fixes #9

--- src/main/kotlin/org/unbrokendome/gradle/plugins/helm/rules/LintTaskRule.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/helm/rules/LintTaskRule.kt
@@ -1,6 +1,7 @@
 package org.unbrokendome.gradle.plugins.helm.rules
 
 import org.gradle.api.tasks.TaskContainer
+import org.unbrokendome.gradle.plugins.helm.HelmPlugin
 import org.unbrokendome.gradle.plugins.helm.command.tasks.HelmLint
 import org.unbrokendome.gradle.plugins.helm.dsl.HelmChart
 import org.unbrokendome.gradle.plugins.helm.dsl.lint
@@ -11,44 +12,45 @@ import org.unbrokendome.gradle.plugins.helm.tasks.HelmFilterSources
  * A rule that creates a [HelmLint] task for each chart.
  */
 internal class LintTaskRule(
-        private val tasks: TaskContainer,
-        private val charts: Iterable<HelmChart>)
-    : AbstractRule() {
+    private val tasks: TaskContainer,
+    private val charts: Iterable<HelmChart>
+) : AbstractRule() {
 
     internal companion object {
         fun getTaskName(chartName: String) =
-                "helmLint${chartName.capitalize()}Chart"
+            "helmLint${chartName.capitalize()}Chart"
     }
 
     private val regex = Regex(getTaskName("(\\p{Upper}.*)"))
 
 
     override fun getDescription(): String =
-            "Pattern: ${getTaskName("<Chart>")}"
+        "Pattern: ${getTaskName("<Chart>")}"
 
 
     override fun apply(taskName: String) {
         if (regex.matches(taskName)) {
             charts.find { it.lintTaskName == taskName }
-                    ?.let { chart ->
+                ?.let { chart ->
 
-                        val filterSourcesTask = tasks.getByName(chart.filterSourcesTaskName) as HelmFilterSources
+                    val filterSourcesTask = tasks.getByName(chart.filterSourcesTaskName) as HelmFilterSources
 
-                        tasks.create(taskName, HelmLint::class.java) { task ->
-                            task.description = "Lints the ${chart.name} chart."
+                    tasks.create(taskName, HelmLint::class.java) { task ->
+                        task.description = "Lints the ${chart.name} chart."
+                        task.dependsOn(HelmPlugin.initClientTaskName)
 
-                            task.chartDir.set(filterSourcesTask.targetDir)
+                        task.chartDir.set(filterSourcesTask.targetDir)
 
-                            chart.lint.let { chartLint ->
-                                task.onlyIf { chartLint.enabled.get() }
-                                task.strict.set(chartLint.strict)
-                                task.values.putAll(chartLint.values)
-                                task.valueFiles.from(chartLint.valueFiles)
-                            }
-
-                            task.dependsOn(chart.updateDependenciesTaskName)
+                        chart.lint.let { chartLint ->
+                            task.onlyIf { chartLint.enabled.get() }
+                            task.strict.set(chartLint.strict)
+                            task.values.putAll(chartLint.values)
+                            task.valueFiles.from(chartLint.valueFiles)
                         }
+
+                        task.dependsOn(chart.updateDependenciesTaskName)
                     }
+                }
         }
     }
 }
