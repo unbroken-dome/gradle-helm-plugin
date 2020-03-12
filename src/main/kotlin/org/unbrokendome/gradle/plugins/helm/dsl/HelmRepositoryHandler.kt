9b2b87164df5ed95762d5ad1ee307a6cdeba6d90
Till Krullmann <till.krullmann@gmx.net>
2020-03-12 21:50:06 +0100

    HelmRepositoryHandler: Add shortcut method to register Bitnami repository
    Also, allow to specify custom name when registering the standard repositories.

--- src/main/kotlin/org/unbrokendome/gradle/plugins/helm/dsl/HelmRepositoryHandler.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/helm/dsl/HelmRepositoryHandler.kt
@@ -2,61 +2,75 @@ package org.unbrokendome.gradle.plugins.helm.dsl
 
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
-import java.net.URI
+
+
+/** The name under which the "stable" repository will be registered. */
+private const val HELM_STABLE_REPOSITORY_NAME = "stable"
+
+/** URL of the Helm "stable" repository. */
+private const val HELM_STABLE_REPOSITORY_URL = "https://kubernetes-charts.storage.googleapis.com/"
+
+/** The name under which the "incubator" repository will be registered. */
+private const val HELM_INCUBATOR_REPOSITORY_NAME = "incubator"
+
+/** URL of the Helm "incubator" repository. */
+private const val HELM_INCUBATOR_REPOSITORY_URL = "https://kubernetes-charts-incubator.storage.googleapis.com/"
+
+/** The name under which the Bitnami repository will be registered. */
+private const val BITNAMI_REPOSITORY_NAME = "bitnami"
+
+/** URL of the Bitnami repository. */
+private const val BITNAMI_REPOSITORY_URL = "https://charts.bitnami.com/bitnami"
 
 
 interface HelmRepositoryHandler : NamedDomainObjectContainer<HelmRepository> {
-
-    companion object {
-
-        /**
-         * The name under which the "stable" repository will be registered.
-         */
-        @JvmStatic
-        val HELM_STABLE_REPOSITORY_NAME = "stable"
-
-        /**
-         * URL of the Helm "stable" repository.
-         */
-        @JvmStatic
-        val HELM_STABLE_REPOSITORY_URL = URI("https://kubernetes-charts.storage.googleapis.com/")
-
-        /**
-         * The name under which the "incubator" repository will be registered.
-         */
-        @JvmStatic
-        val HELM_INCUBATOR_REPOSITORY_NAME = "incubator"
-
-        /**
-         * URL of the Helm "incubator" repository.
-         */
-        @JvmStatic
-        val HELM_INCUBATOR_REPOSITORY_URL = URI("https://kubernetes-charts-incubator.storage.googleapis.com/")
-    }
-
 
     /**
      * Registers the Helm "stable" repository using the name "stable".
      *
      * The sources of the charts in the repository can be found at [https://github.com/helm/charts].
+     *
+     * @param name the name under which to register the repository, defaults to "stable"
      */
     @JvmDefault
-    fun helmStable(): HelmRepository =
-        create(HELM_STABLE_REPOSITORY_NAME) { repo ->
-            repo.url.set(HELM_STABLE_REPOSITORY_URL)
+    @Suppress("unused")
+    fun helmStable(name: String = HELM_STABLE_REPOSITORY_NAME) {
+        register(name) { repo ->
+            repo.url(HELM_STABLE_REPOSITORY_URL)
         }
+    }
 
 
     /**
      * Registers the Helm "incubator" repository using the name "incubator".
      *
      * The sources of the charts in the repository can be found at [https://github.com/helm/charts].
+     *
+     * @param name the name under which to register the repository, defaults to "incubator"
      */
     @JvmDefault
-    fun helmIncubator(): HelmRepository =
-        create(HELM_INCUBATOR_REPOSITORY_NAME) { repo ->
-            repo.url.set(HELM_INCUBATOR_REPOSITORY_URL)
+    @Suppress("unused")
+    fun helmIncubator(name: String = HELM_INCUBATOR_REPOSITORY_NAME) {
+        register(name) { repo ->
+            repo.url(HELM_INCUBATOR_REPOSITORY_URL)
         }
+    }
+
+
+    /**
+     * Registers the Bitnami repository.
+     *
+     * The sources of the charts in the repository can be found at [https://github.com/helm/charts].
+     *
+     * @param name the name under which to register the repository, defaults to "bitnami"
+     */
+    @JvmDefault
+    @Suppress("unused")
+    fun bitnami(name: String = BITNAMI_REPOSITORY_NAME) {
+        register(name) { repo ->
+            repo.url(BITNAMI_REPOSITORY_URL)
+        }
+    }
 }
 
 
