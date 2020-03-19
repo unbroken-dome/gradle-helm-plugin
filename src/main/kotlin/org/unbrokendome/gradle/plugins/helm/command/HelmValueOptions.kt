a362cd0cc1655426c13b85ff1aa48e98b02cbaad
Till Krullmann <till.krullmann@gmx.net>
2020-03-19 11:12:22 +0100

    HelmValueOptions: allow different types in `fileValues` and resolve providers and other deferred types, instead of just calling `toString()`

--- src/main/kotlin/org/unbrokendome/gradle/plugins/helm/command/HelmValueOptions.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/helm/command/HelmValueOptions.kt
@@ -1,10 +1,14 @@
 package org.unbrokendome.gradle.plugins.helm.command
 
+import groovy.lang.Closure
 import org.gradle.api.Project
 import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.FileCollection
 import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Provider
+import org.gradle.api.resources.TextResource
 import org.slf4j.LoggerFactory
+import java.util.concurrent.Callable
 
 
 interface HelmValueOptions {
@@ -24,7 +28,7 @@ interface HelmValueOptions {
      * Corresponds to the `--set-file` CLI option.
      *
      * The values of the map can be of any type that is accepted by [Project.file]. Additionally, when adding a
-     * [Provider] that represents an output file of another task, this task will automatically have a task
+     * [Provider] that represents an output file of another task, the consuming task will automatically have a task
      * dependency on the producing task.
      *
      * Not to be confused with [valueFiles], which contains a collection of YAML files that supply multiple values.
@@ -70,9 +74,20 @@ internal object HelmValueOptionsApplier : HelmOptionsApplier {
 
                 option("--set-file",
                     options.fileValues.map<String> { m ->
+
                         @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
                         m.takeIf { it.isNotEmpty() }
-                            ?.entries?.joinToString(separator = ",") { (key, value) -> "$key=$value" }
+                            ?.entries?.joinToString(separator = ",") { (key, value) ->
+
+                            val valueRepresentation = when (val resolvedValue = resolveValue(value)) {
+                                is FileCollection -> resolvedValue.singleFile
+                                is TextResource -> resolvedValue.asFile()
+                                else -> resolvedValue
+                            }
+
+                            "$key=$valueRepresentation"
+
+                        }
                     }
                 )
 
@@ -83,4 +98,13 @@ internal object HelmValueOptionsApplier : HelmOptionsApplier {
             }
         }
     }
+
+
+    private fun resolveValue(value: Any?): Any? =
+        when (value) {
+            is Provider<*> -> resolveValue(value.orNull)
+            is Callable<*> -> resolveValue(value.call())
+            is Closure<*> -> resolveValue(value.call())
+            else -> value
+        }
 }
