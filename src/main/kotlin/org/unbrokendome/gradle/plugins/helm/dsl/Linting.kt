383633f1fa23033b61da73b197709ba6290e36e6
Till Krullmann <till.krullmann@gmx.net>
2020-09-24 17:29:09 +0200

    Multiple lint configurations

--- src/main/kotlin/org/unbrokendome/gradle/plugins/helm/dsl/Linting.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/helm/dsl/Linting.kt
@@ -1,15 +1,31 @@
 package org.unbrokendome.gradle.plugins.helm.dsl
 
+import org.gradle.api.Action
+import org.gradle.api.Named
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.unbrokendome.gradle.plugins.helm.command.ConfigurableHelmValueOptions
 import org.unbrokendome.gradle.plugins.helm.command.mergeValues
+import javax.inject.Inject
 
 
 /**
  * Defines options for linting Helm charts using the `helm lint` command.
  */
 interface Linting : ConfigurableHelmValueOptions {
+
+    /**
+     * A configuration of values to pass to `helm lint`.
+     */
+    interface Configuration : Named, ConfigurableHelmValueOptions {
+
+        companion object {
+            @JvmStatic
+            val DEFAULT_CONFIGURATION_NAME = "default"
+        }
+    }
+
 
     /**
      * If `true` (the default), run the linter.
@@ -22,6 +38,31 @@ interface Linting : ConfigurableHelmValueOptions {
      * Corresponds to the `--strict` CLI option.
      */
     val strict: Property<Boolean>
+
+    /**
+     * A collection of linter configurations.
+     *
+     * Each configuration allows specifying a different set of values to pass to `helm lint`. Values defined in
+     * the `Linting` block directly are merged into all configurations.
+     *
+     * If no configurations are added to this container, the plugin will assume a single configuration named
+     * "default" with no additional values.
+     */
+    val configurations: NamedDomainObjectContainer<Configuration>
+
+
+    /**
+     * Configures the linter configurations.
+     *
+     * Each configuration allows specifying a different set of values to pass to `helm lint`. Values defined in
+     * the `Linting` block directly are merged into all configurations.
+     *
+     * If no configurations are added to this container, the plugin will assume a single configuration named
+     * "default" with no additional values.
+     */
+    @JvmDefault
+    fun configurations(configureAction: Action<NamedDomainObjectContainer<Configuration>>) =
+        configurations.apply(configureAction::execute)
 }
 
 
@@ -29,6 +70,29 @@ internal fun Linting.setParent(parent: Linting) {
     mergeValues(parent)
     enabled.set(parent.enabled)
     strict.set(parent.strict)
+}
+
+
+// Unfortunately Gradle isn't smart enough (yet) to implement the Named interface for us,
+// otherwise we could just use the interfaces here
+private abstract class DefaultLintConfiguration
+@Inject constructor(
+    private val name: String
+) : Linting.Configuration {
+
+    override fun getName(): String = name
+}
+
+
+private abstract class DefaultLinting
+@Inject constructor(
+    objects: ObjectFactory
+) : Linting {
+
+    final override val configurations: NamedDomainObjectContainer<Linting.Configuration> =
+        objects.domainObjectContainer(Linting.Configuration::class.java) { name ->
+            objects.newInstance(DefaultLintConfiguration::class.java, name)
+        }
 }
 
 
@@ -40,8 +104,16 @@ internal fun Linting.setParent(parent: Linting) {
  * @return the created [Linting] object
  */
 internal fun ObjectFactory.createLinting(parent: Linting? = null): Linting =
-    newInstance(Linting::class.java)
+    newInstance(DefaultLinting::class.java)
         .apply {
             enabled.convention(true)
+
+            // Add a rule that creates a "default" lint configuration if the user didn't add any
+            configurations.addRule("default lint configuration") { configurationName ->
+                if (configurations.isEmpty() && configurationName == Linting.Configuration.DEFAULT_CONFIGURATION_NAME) {
+                    configurations.create(Linting.Configuration.DEFAULT_CONFIGURATION_NAME)
+                }
+            }
+
             parent?.let(this::setParent)
         }
--- src/main/kotlin/org/unbrokendome/gradle/plugins/helm/rules/LintTaskRule.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/helm/rules/LintTaskRule.kt
@@ -1,11 +1,14 @@
 package org.unbrokendome.gradle.plugins.helm.rules
 
 import org.gradle.api.NamedDomainObjectCollection
+import org.gradle.api.Task
 import org.gradle.api.tasks.TaskContainer
+import org.gradle.api.tasks.TaskDependency
 import org.gradle.kotlin.dsl.lint
-import org.unbrokendome.gradle.plugins.helm.command.mergeValues
+import org.unbrokendome.gradle.plugins.helm.HELM_GROUP
 import org.unbrokendome.gradle.plugins.helm.command.tasks.HelmLint
 import org.unbrokendome.gradle.plugins.helm.dsl.HelmChart
+import org.unbrokendome.gradle.plugins.helm.dsl.Linting
 
 
 private val namePattern =
@@ -13,36 +16,40 @@ private val namePattern =
 
 
 /**
- * The name of the [HelmLint] task for this chart.
+ * The name of the task that performs all linting for this chart.
  */
 val HelmChart.lintTaskName
     get() = namePattern.mapName(name)
 
 
 /**
- * A rule that creates a [HelmLint] task for each chart.
+ * A rule that creates a lint task for each chart.
+ *
+ * - If the chart does not have lint configurations, creates task is of type [HelmLint]
+ * - If the chart has lint configurations, creates a simple [Task] that depends on all configuration-specific
+ *   [HelmLint] tasks.
  */
 internal class LintTaskRule(
     tasks: TaskContainer,
     charts: NamedDomainObjectCollection<HelmChart>
-) : AbstractHelmChartTaskRule<HelmLint>(
-    HelmLint::class.java, tasks, charts, namePattern
-) {
+) : AbstractHelmChartTaskRule<Task>(Task::class.java, tasks, charts, namePattern) {
 
-    override fun HelmLint.configureFrom(chart: HelmChart) {
+    override fun Task.configureFrom(chart: HelmChart) {
 
         description = "Lints the ${chart.name} chart."
+        group = HELM_GROUP
 
-        chartDir.set(chart.outputDir)
+        val chartLint = chart.lint
 
-        chart.lint.let { chartLint ->
-            onlyIf { chartLint.enabled.get() }
-            strict.set(chartLint.strict)
-            mergeValues(chartLint)
-        }
+        onlyIf { chartLint.enabled.get() }
 
-        dependsOn(
-            chart.updateDependenciesTaskName
-        )
+        // poke the "default" configuration so it is created if we don't have any configurations
+        chartLint.configurations.findByName(Linting.Configuration.DEFAULT_CONFIGURATION_NAME)
+
+        dependsOn(TaskDependency {
+            chartLint.configurations.map { lintConfiguration ->
+                tasks.getByName(chart.lintTaskName(lintConfiguration.name))
+            }.toSet()
+        })
     }
 }
