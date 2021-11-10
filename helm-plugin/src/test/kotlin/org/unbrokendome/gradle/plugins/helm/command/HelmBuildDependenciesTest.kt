aa4dbc305d64112579d96bbf66e97aab1f97aef2
Till Krullmann <till.krullmann@gmail.com>
2021-11-10 17:00:00 +0100

    Fix tests of HelmBuildDependencies and HelmUpdateDependencies to also consider the onlyIf conditions

--- helm-plugin/src/test/kotlin/org/unbrokendome/gradle/plugins/helm/command/HelmBuildDependenciesTest.kt
+++ helm-plugin/src/test/kotlin/org/unbrokendome/gradle/plugins/helm/command/HelmBuildDependenciesTest.kt
@@ -1,10 +1,15 @@
 package org.unbrokendome.gradle.plugins.helm.command
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import org.spekframework.spek2.style.specification.describe
 import org.unbrokendome.gradle.plugins.helm.command.tasks.HelmBuildDependencies
 import org.unbrokendome.gradle.plugins.helm.spek.ExecutionResultAwareSpek
 import org.unbrokendome.gradle.plugins.helm.spek.gradleExecMock
 import org.unbrokendome.gradle.plugins.helm.testutil.exec.singleInvocation
+import org.unbrokendome.gradle.plugins.helm.testutil.exec.verifyNoInvocations
+import org.unbrokendome.gradle.pluginutils.test.TaskOutcome
+import org.unbrokendome.gradle.pluginutils.test.directory
 import org.unbrokendome.gradle.pluginutils.test.execute
 import org.unbrokendome.gradle.pluginutils.test.spek.applyPlugin
 import org.unbrokendome.gradle.pluginutils.test.spek.gradleTask
@@ -27,6 +32,30 @@ object HelmBuildDependenciesTest : ExecutionResultAwareSpek({
 
             it("should execute helm dependency build") {
 
+                task.execute(checkOnlyIf = false)
+
+                execMock.singleInvocation {
+                    expectCommand("dependency", "build")
+                    expectArg("${project.projectDir}/src")
+                }
+            }
+        }
+    }
+
+    describe("when lock file exists (v1 API)") {
+
+        beforeEachTest {
+            directory(project.projectDir) {
+                directory("src") {
+                    file("Chart.yaml", "apiVersion: v1")
+                    file("requirements.lock", "")
+                }
+            }
+        }
+
+        describe("executing a HelmBuildDependencies task") {
+            it("should execute helm dependency build") {
+
                 task.execute()
 
                 execMock.singleInvocation {
@@ -35,5 +64,85 @@ object HelmBuildDependenciesTest : ExecutionResultAwareSpek({
                 }
             }
         }
+    }
+
+    describe("when lock file exists (v2 API)") {
+
+        beforeEachTest {
+            directory(project.projectDir) {
+                directory("src") {
+                    file("Chart.yaml", "apiVersion: v2")
+                    file("Chart.lock", "")
+                }
+            }
+        }
+
+        describe("executing a HelmBuildDependencies task") {
+            it("should execute helm dependency build") {
+
+                task.execute()
+
+                execMock.singleInvocation {
+                    expectCommand("dependency", "build")
+                    expectArg("${project.projectDir}/src")
+                }
+            }
+        }
+    }
+
+    describe("when lock file does not exist") {
+
+        describe("when chart has no dependencies") {
+            beforeEachTest {
+                directory(project.projectDir) {
+                    directory("src") {
+                        file("Chart.yaml",
+                            """
+                            |apiVersion: v2
+                            """.trimMargin())
+                    }
+                }
+            }
+
+            describe("executing a HelmBuildDependencies task") {
+                it("should not execute helm dependency build") {
+
+                    val result = task.execute()
+                    assertThat(result, "result").isEqualTo(TaskOutcome.SKIPPED)
+
+                    execMock.verifyNoInvocations()
+                }
+            }
+        }
+
+        describe("when chart has external dependencies") {
+            beforeEachTest {
+                directory(project.projectDir) {
+                    directory("src") {
+                        file("Chart.yaml",
+                            """
+                            |apiVersion: v2
+                            |dependencies:
+                            |  - name: foo
+                            |    version: "1.2.3"
+                            |    repository: https://my.example.repo
+                            """.trimMargin())
+                    }
+                }
+            }
+
+            describe("executing a HelmBuildDependencies task") {
+                it("should execute helm dependency build") {
+
+                    task.execute()
+
+                    execMock.singleInvocation {
+                        expectCommand("dependency", "build")
+                        expectArg("${project.projectDir}/src")
+                    }
+                }
+            }
+        }
+
     }
 })
--- helm-plugin/src/test/kotlin/org/unbrokendome/gradle/plugins/helm/command/HelmUpdateDependenciesTest.kt
+++ helm-plugin/src/test/kotlin/org/unbrokendome/gradle/plugins/helm/command/HelmUpdateDependenciesTest.kt
@@ -1,10 +1,15 @@
 package org.unbrokendome.gradle.plugins.helm.command
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import org.spekframework.spek2.style.specification.describe
 import org.unbrokendome.gradle.plugins.helm.command.tasks.HelmUpdateDependencies
 import org.unbrokendome.gradle.plugins.helm.spek.ExecutionResultAwareSpek
 import org.unbrokendome.gradle.plugins.helm.spek.gradleExecMock
 import org.unbrokendome.gradle.plugins.helm.testutil.exec.singleInvocation
+import org.unbrokendome.gradle.plugins.helm.testutil.exec.verifyNoInvocations
+import org.unbrokendome.gradle.pluginutils.test.TaskOutcome
+import org.unbrokendome.gradle.pluginutils.test.directory
 import org.unbrokendome.gradle.pluginutils.test.execute
 import org.unbrokendome.gradle.pluginutils.test.spek.applyPlugin
 import org.unbrokendome.gradle.pluginutils.test.spek.gradleTask
@@ -27,6 +32,40 @@ object HelmUpdateDependenciesTest : ExecutionResultAwareSpek({
 
             it("should execute helm dependency update") {
 
+                task.execute(checkOnlyIf = false)
+
+                execMock.singleInvocation {
+                    expectCommand("dependency", "update")
+                    expectArg("${project.projectDir}/src")
+                }
+            }
+        }
+    }
+
+
+    describe("when chart has external dependencies") {
+
+        beforeEachTest {
+            directory(project.projectDir) {
+                directory("src") {
+                    file(
+                        "Chart.yaml",
+                        """
+                            |apiVersion: v2
+                            |dependencies:
+                            |  - name: foo
+                            |    version: "1.2.3"
+                            |    repository: https://my.example.repo
+                            """.trimMargin()
+                    )
+                }
+            }
+        }
+
+        describe("when executing a HelmUpdateDependencies task") {
+
+            it("should execute helm dependency update") {
+
                 task.execute()
 
                 execMock.singleInvocation {
@@ -34,7 +73,6 @@ object HelmUpdateDependenciesTest : ExecutionResultAwareSpek({
                     expectArg("${project.projectDir}/src")
                 }
             }
-
 
             it("should use skipRefresh property") {
 
@@ -48,6 +86,30 @@ object HelmUpdateDependenciesTest : ExecutionResultAwareSpek({
                     expectArg("${project.projectDir}/src")
                 }
             }
+        }
+    }
+
+    describe("when chart has no external dependencies") {
+
+        beforeEachTest {
+            directory(project.projectDir) {
+                directory("src") {
+                    file(
+                        "Chart.yaml",
+                        """
+                            |apiVersion: v2
+                            """.trimMargin()
+                    )
+                }
+            }
+        }
+
+        it("should skip helm dependency update") {
+
+            val result = task.execute()
+            assertThat(result, "result").isEqualTo(TaskOutcome.SKIPPED)
+
+            execMock.verifyNoInvocations()
         }
     }
 })
