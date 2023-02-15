210824f20dd5f49058a42cf00d322b85f851ce64
Igor Manushin <igor.manushin@gmail.com>
2023-02-15 20:20:26 +0000

    update gradle (and Kotlin), remove some deprecated API

--- helm-publish-plugin/src/main/kotlin/org/unbrokendome/gradle/plugins/helm/publishing/tasks/HelmPublishChart.kt
+++ helm-publish-plugin/src/main/kotlin/org/unbrokendome/gradle/plugins/helm/publishing/tasks/HelmPublishChart.kt
@@ -7,8 +7,6 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
-import org.gradle.util.GradleVersion
-import org.gradle.workers.IsolationMode
 import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
 import org.gradle.workers.WorkerExecutor
@@ -16,11 +14,7 @@ import org.unbrokendome.gradle.plugins.helm.HELM_GROUP
 import org.unbrokendome.gradle.plugins.helm.publishing.dsl.HelmPublishingRepository
 import org.unbrokendome.gradle.plugins.helm.publishing.dsl.HelmPublishingRepositoryInternal
 import org.unbrokendome.gradle.plugins.helm.publishing.publishers.PublisherParams
-import org.unbrokendome.gradle.pluginutils.GradleVersions
 import org.unbrokendome.gradle.pluginutils.property
-import java.io.File
-import java.io.Serializable
-import java.util.concurrent.ExecutionException
 import javax.inject.Inject
 
 
@@ -31,12 +25,6 @@ open class HelmPublishChart
 @Inject constructor(
     private val workerExecutor: WorkerExecutor
 ) : DefaultTask() {
-
-    private companion object {
-
-        val NEW_WORKER_API_GRADLE_VERSION: GradleVersion = GradleVersions.Version_5_6
-    }
-
 
     init {
         group = HELM_GROUP
@@ -85,31 +73,11 @@ open class HelmPublishChart
                 (targetRepository as HelmPublishingRepositoryInternal).publisherParams
             }
 
-        if (GradleVersion.current() >= NEW_WORKER_API_GRADLE_VERSION) {
-
-            workerExecutor.noIsolation().submit(PublishChartWorkAction::class.java) { params ->
-                params.chartName.set(chartName)
-                params.chartVersion.set(chartVersion)
-                params.chartFile.set(chartFile)
-                params.publisherParams.set(publisherParams)
-            }
-
-        } else {
-
-            // legacy worker API for pre-5.6 compatibility
-
-            val params = PublishChartLegacyParams(
-                chartName = chartName.get(),
-                chartVersion = chartVersion.get(),
-                chartFile = chartFile.get().asFile,
-                publisherParams = publisherParams
-            )
-
-            @Suppress("DEPRECATION")
-            workerExecutor.submit(PublishChartWorker::class.java) { config ->
-                config.isolationMode = IsolationMode.NONE
-                config.setParams(params)
-            }
+        workerExecutor.noIsolation().submit(PublishChartWorkAction::class.java) { params ->
+            params.chartName.set(chartName)
+            params.chartVersion.set(chartVersion)
+            params.chartFile.set(chartFile)
+            params.publisherParams.set(publisherParams)
         }
     }
 }
@@ -147,33 +115,3 @@ internal abstract class PublishChartWorkAction : WorkAction<PublishChartWorkPara
     }
 }
 
-
-internal class PublishChartLegacyParams(
-    val chartName: String,
-    val chartVersion: String,
-    val chartFile: File,
-    val publisherParams: PublisherParams
-) : Serializable
-
-
-internal class PublishChartWorker
-@Inject constructor(
-    private val params: PublishChartLegacyParams
-) : Runnable {
-
-    override fun run() {
-
-        val publisher = params.publisherParams.createPublisher()
-
-        try {
-            publisher.publish(
-                chartName = params.chartName,
-                chartVersion = params.chartVersion,
-                chartFile = params.chartFile
-            )
-
-        } catch (ex: ExecutionException) {
-            ex.cause?.printStackTrace()
-        }
-    }
-}
