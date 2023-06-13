f23417d2b2eede45c0a361c33ffa7c4904c8624a
imanushin <igor.manushin@citi.com>
2023-06-13 14:12:06 +0100

    Add dependency updates plugin
    Signed-off-by: imanushin <igor.manushin@citi.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.gradle.plugin-publish") version "0.14.0" apply false
     id("org.jetbrains.dokka") version "1.4.32"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 
