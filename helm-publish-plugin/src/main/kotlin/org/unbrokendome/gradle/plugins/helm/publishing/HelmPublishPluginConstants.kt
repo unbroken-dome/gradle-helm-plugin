922576514b33301b74225ec420749a1b3b66033c
Till Krullmann <till.krullmann@symmedia.de>
2023-08-20 22:07:17 +0200

    Migrate chart publish convention property to an extension object on chart

--- helm-publish-plugin/src/main/kotlin/org/unbrokendome/gradle/plugins/helm/publishing/HelmPublishPluginConstants.kt
+++ helm-publish-plugin/src/main/kotlin/org/unbrokendome/gradle/plugins/helm/publishing/HelmPublishPluginConstants.kt
@@ -14,9 +14,6 @@ internal const val HELM_PUBLISHING_REPOSITORIES_EXTENSION_NAME = "repositories"
 
 
 /**
- * The name of the publishing convention object installed on each chart.
- *
- * Note that since convention properties are accessible directly on the object, the name of the convention
- * does not really matter except for uniqueness.
+ * The name of the publishing extension object installed on each chart.
  */
-const val HELM_CHART_PUBLISHING_CONVENTION_NAME = "publishing"
+internal const val HELM_CHART_PUBLISHING_EXTENSION_NAME = "publishing"
